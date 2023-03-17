c61394e4aba782b6a4eae2bf288174fe25fd5b1e
maiaferrer <mferrer2@uncc.edu>
2023-03-17 14:04:06 -0400

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className="bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-amber-200 via-violet-600 to-sky-900">About</div>
+    <section>
+      <h2>about the developer</h2>
+         <p>My name is Maia and I am a fellow creative based out of Charlotte, NC. I am a kinesthetic learner who enjoys building and designing on the web. Web development granted me a creative outlet in which I have been able to fulfill through innovative debugging, algorithm building, and implementing intuitive UI.<br></br>
+            On my free time I enjoy being outdoors with my fur babies, photography, creating graphic art, and drinking coffee.
+            I am currently enrolled in UNC Charlottes Coding Bootcamp and am on the path to becoming a FullStack
+            Developer.<br></br>
+            </p>
+            <img class="image" src="#" alt="a picture of Maia"/>
+    </section>
+    
   )
 }
