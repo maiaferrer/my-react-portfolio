eb616354d37490744f8459fb3b774c792432aec4
maiaferrer <mferrer2@uncc.edu>
2023-04-04 20:25:22 -0400

    added readme and icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Icon } from '@iconify/react';
 
 export default function Contact() {
   return (
     <div className="">
       <h1>Contact</h1>
+      <ul>
+        <li>
+          <Icon icon="ri:github-fill" />
+        </li>
+      </ul>
+      
       </div>
   )
 }
